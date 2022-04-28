b5f3edefb5b6ec24c286faf29cd889a2d858ebed
Gill Johny <bgroholsky@gmail.com>
2022-04-28 19:54:36 +0300

    install query

--- Front/src/app/components/list-ads/list-ads.component.ts
+++ Front/src/app/components/list-ads/list-ads.component.ts
@@ -11,6 +11,34 @@ export class ListAdsComponent implements OnInit {
 
   ngOnInit(): void {
 
+
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    if(output != null && slider != null){
+    
+    output.innerHTML =  slider.getAttribute("value") + "";
+    
+    slider.oninput = function() {
+      let a : any = this;
+      console.log(a.value)
+      /*
+      console.log(event.target);
+      console.log(event.target.getAttribute("value"));
+      if(output != null)
+      output.innerHTML = event.target.getAttribute("value");
+      */
+    }
+  }
+
+  }
+
+  test(event : any){
+
+
+    let a = this;
+    console.log(event.target)
+    let slider = document.getElementById("myRange");   
+  console.log(event.target.getAttribute("value"));
   }
 
   openCloseTaggle(event : any){
