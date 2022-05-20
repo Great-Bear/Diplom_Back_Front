2b1a256b6533ff57781803629aa33741742a79b9
Gill Johny <bgroholsky@gmail.com>
2022-05-20 21:37:16 +0300

    android app

--- Front/src/app/http.service.ts
+++ Front/src/app/http.service.ts
@@ -28,7 +28,7 @@ export class HttpService {
 
   // https://apijbs.azurewebsites.net
 //   https://localhost:44398
-  private URL : string = "https://localhost:44398";
+  private URL : string = "https://apijbs.azurewebsites.net";
 
    getData() {
       const response = this.http.get("/api/auth/TestMeth");
